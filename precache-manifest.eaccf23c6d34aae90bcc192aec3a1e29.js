933321b7b5940e2eb3a47201e7c5aeace1020196
Medasele273 <medasele273@gmail.com>
2019-11-24 18:46:18 +0100

    Updates

---
+++ precache-manifest.eaccf23c6d34aae90bcc192aec3a1e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7945a804995fb64497226b3ec86f26",
+    "url": "/javafriends/index.html"
+  },
+  {
+    "revision": "8843c24fc540323b5bee",
+    "url": "/javafriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "36b7cfef3a6517f9dfb0",
+    "url": "/javafriends/static/css/main.7a0500e2.chunk.css"
+  },
+  {
+    "revision": "8843c24fc540323b5bee",
+    "url": "/javafriends/static/js/2.b63546f1.chunk.js"
+  },
+  {
+    "revision": "36b7cfef3a6517f9dfb0",
+    "url": "/javafriends/static/js/main.80d88777.chunk.js"
+  },
+  {
+    "revision": "c94b9ed4ce8d95a6f8ea",
+    "url": "/javafriends/static/js/runtime-main.a3df7f64.js"
+  }
+]);
